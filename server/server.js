fdf44f44e57c4bb77a92f57715e8f17d3b15b10f
Darrell Pratt <darrell.pratt@gmail.com>
2014-03-19 11:21:51 -0500

    fixed html to take enter button

--- server/server.js
+++ server/server.js
@@ -6,9 +6,8 @@ var app = express();
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, '../client/app')));
 
-// app.get('/cr/:id/reports', employees.findByManager);
 app.get('/api/cr/:id', cr.findById);
-// app.get('/employees', employees.findAll);
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
