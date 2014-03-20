9aa2a1261736002bba866eae049b5623faaebc55
Darrell Pratt <darrell.pratt@gmail.com>
2014-03-20 14:06:57 -0500

    formatting

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,21 +1,21 @@
 'use strict';
 
 angular.module('app')
-  .controller('MainController', function ($scope, $routeParams, $window, ChangeRequest) {
+.controller('MainController', function ($scope, $routeParams, $window, ChangeRequest) {
 
-    $scope.search = function () {
-      // Get CR ID ??
-      var cr;
-      if ($scope.id === Number.NaN) {
-        $scope.cr = {};
-      } else {
-        cr = ChangeRequest.get({'id': $scope.id});
-        $scope.cr = cr;
-        // $scope.post = Post.get({id: $routeParams.id}, success);
-        console.log(cr);
-        // console.log('id: ' + cr.id);
-        // $window.localStorage.setItem('test', JSON.stringify(cr));
-      }
+  $scope.search = function () {
+    // Get CR ID ??
+    var cr;
+    if ($scope.id === Number.NaN) {
+      $scope.cr = {};
+    } else {
+      cr = ChangeRequest.get({'id': $scope.id});
+      $scope.cr = cr;
+      // $scope.post = Post.get({id: $routeParams.id}, success);
+      console.log(cr);
+      // console.log('id: ' + cr.id);
+      // $window.localStorage.setItem('test', JSON.stringify(cr));
+    }
 
-    };
-  });
+  };
+});
--- client/app/scripts/resources/cr.js
+++ client/app/scripts/resources/cr.js
@@ -26,5 +26,5 @@ window.App.factory('ChangeRequest', function($resource) {
 
   var ChangeRequest = $resource('/api/cr/:id', { id:'@id' });
   return ChangeRequest;
-  
+
 });
