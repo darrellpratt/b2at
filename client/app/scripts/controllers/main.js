70054ec534f47934fddf20260bd498cc30a982c8
Darrell Pratt <darrell.pratt@gmail.com>
2014-08-11 11:59:45 -0500

    adding string.js

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,116 +2,142 @@
 
 
 angular.module('app')
-.controller('MainController', function ($timeout, $location, $routeParams, 
-                                        $resource, $scope, $window, 
-                                        ChangeRequest, SlackMessage, 
-                                        bucketStorage, $firebase,
-                                        Couchbase) {
+    .controller('MainController', function($timeout, $location, $routeParams,
+        $resource, $scope, $window,
+        ChangeRequest, SlackMessage,
+        bucketStorage, $firebase,
+        Couchbase) {
 
-  // load our local storage
-  $scope.bucket = bucketStorage.get();
-  $scope.bucketList = [];
-  // build track list from the object array if there
-  _.each($scope.bucket, function(i) {
-    $scope.bucketList.unshift(i.id);
-  })
+        // load our local storage
+        $scope.bucket = bucketStorage.get();
+        $scope.bucketList = [];
+        // build track list from the object array if there
+        _.each($scope.bucket, function(i) {
+            $scope.bucketList.unshift(i.id);
+        });
 
-  // real time messages from firebase on favorite activity
-  var ref = new Firebase("https://amber-fire-1059.firebaseio.com/");
-  ref.endAt().limit(5);
-  $scope.messages = $firebase(ref);
-  console.log($scope.messages);
 
-  console.log($routeParams);
+        // real time messages from firebase on favorite activity
+        var ref = new Firebase("https://amber-fire-1059.firebaseio.com/");
+        $scope.messages = $firebase(ref.limit(10)).$asArray();
+        // console.log($firebase(ref).$asObject());
+        // var fireObject = $firebase(ref).$asObject();
+        // fireObject.$bindTo($scope, "fires");
 
-  if ($routeParams.crid) {
-    $scope.id = $routeParams.crid;
-    console.log($scope.id);
-    if ($scope.id === Number.NaN || $scope.id === '') {
-      $scope.cr = {};
-    } else {
-      ChangeRequest.get({'id': $scope.id}, function(cr) {
-        $scope.cr = cr;
-      });
+        console.log($scope.messages);
+        
 
-  };
-}
 
-  $scope.search = function () {
-    console.log('into search');
-    if ($scope.id === Number.NaN || $scope.id === '') {
-      $scope.cr = {};
-    } else {
-      ChangeRequest.get({'id': $scope.id}, function(cr) {
-        $scope.cr = cr;
-        // $routeParams.crid = $scope.id;
-        $timeout(function () {
-          console.log($location.path());
-          $location.path('/cr/' + $scope.id);
-          console.log($scope);
-          console.log('scope: ' + $scope.id + ' route: ' + $routeParams.crid);
-        }, 4000);
 
-      });
-    };
-  };
+        if ($routeParams.crid) {
+            $scope.id = $routeParams.crid;
+            console.log($scope.id);
+            if ($scope.id === Number.NaN || $scope.id === '') {
+                $scope.cr = {};
+            } else {
+                ChangeRequest.get({
+                    'id': $scope.id
+                }, function(cr) {
+                    $scope.cr = cr;
+                });
 
-  $scope.save = function (id) {
-    if (_.indexOf($scope.bucketList, $scope.cr.id) < 0) {
-      $scope.bucket.unshift($scope.cr);
-      $scope.bucketList.unshift(id);
-      bucketStorage.remove();
-      bucketStorage.put($scope.bucket);
-      var msg = $scope.cr.title + " was favorited";
-      $scope.messages.$add({from: 'b2at bot', body: msg});
-    } else {
+            };
+        }
 
-      var i = $scope.bucketList.indexOf(id);
-      $scope.bucketList.splice(i, 1);
-      $scope.bucket.splice(i, 1);
-      bucketStorage.remove();
-      bucketStorage.put($scope.bucket);
+        $scope.search = function() {
+            console.log('into search');
+            if ($scope.id === Number.NaN || $scope.id === '') {
+                $scope.cr = {};
+            } else {
+                ChangeRequest.get({
+                    'id': $scope.id
+                }, function(cr) {
+                    $scope.cr = cr;
+                    // $routeParams.crid = $scope.id;
+                    $timeout(function() {
+                        console.log($location.path());
+                        $location.path('/cr/' + $scope.id);
+                        console.log($scope);
+                        console.log('scope: ' + $scope.id + ' route: ' + $routeParams.crid);
+                    }, 4000);
 
-    };
-    console.log($scope.bucketList);
-    // console.log(_.sortBy($scope.bucket, 'id'));
-    console.log($scope.bucket);
-  };
+                });
+            };
+        };
 
-  $scope.load = function(itemId) {
-    // console.log($scope.itemId);
-    console.log(itemId);
-    $scope.id = itemId;
-    $window.document.body.scrollTop = 0;
-    $location.path('/cr/' + $scope.id);
-  };
+        $scope.save = function(id) {
+            if (_.indexOf($scope.bucketList, $scope.cr.id) < 0) {
+                $scope.bucket.unshift($scope.cr);
+                $scope.bucketList.unshift(id);
+                bucketStorage.remove();
+                bucketStorage.put($scope.bucket);
+                var msg = $scope.cr.title + " was favorited";
+                $scope.messages.$add({
+                    from: 'b2at bot',
+                    body: msg
+                });
+            } else {
 
-  $scope.addMessage = function(e) {
-    if (e.keyCode != 13) return;
-    $scope.messages.$add({from: $scope.name, body: $scope.msg});
-    $scope.msg = "";
-  };
+                var i = $scope.bucketList.indexOf(id);
+                $scope.bucketList.splice(i, 1);
+                $scope.bucket.splice(i, 1);
+                bucketStorage.remove();
+                bucketStorage.put($scope.bucket);
 
-  $scope.slack = function(itemId) {
-    console.log(itemId);
-    $scope.id = itemId;
-    SlackMessage.put({'id': itemId}, function(cr) {
-        console.log('done');
+            };
+            console.log($scope.bucketList);
+            // console.log(_.sortBy($scope.bucket, 'id'));
+            console.log($scope.bucket);
+        };
+
+        $scope.load = function(itemId) {
+            // console.log($scope.itemId);
+            var rawItem = $window.S(itemId);
+
+
+            var rawId = rawItem.toInt();
+            console.log(rawId);
+            console.log(itemId);
+            $scope.id = rawId;
+            $window.document.body.scrollTop = 0;
+            $location.path('/cr/' + $scope.id);
+
+        };
+
+
+        $scope.addMessage = function(e) {
+            if (e.keyCode != 13) return;
+            $scope.messages.$add({
+                from: $scope.name,
+                body: $scope.msg
+            });
+            $scope.msg = "";
+        };
+
+        $scope.slack = function(itemId) {
+            console.log(itemId);
+            $scope.id = itemId;
+            SlackMessage.put({
+                'id': itemId
+            }, function(cr) {
+                console.log('done');
+
+            });
+        };
+
+        $scope.couch = function() {
+            var itemId = $scope.id;
+            console.log('couch delete on: ' + itemId);
+            Couchbase.delete({
+                'id': itemId
+            }, function(item) {
+                console.log('deleted item: ' + item);
+                $scope.search();
+            });
+
+        };
+
+
+
 
     });
-  };
-
-  $scope.couch = function() {
-    var itemId = $scope.id;
-    console.log('couch delete on: ' + itemId);
-    Couchbase.delete({'id' : itemId}, function(item){
-      console.log('deleted item: ' + item);
-      $scope.search();
-    });
-
-  };
-
-
-
-
-});
